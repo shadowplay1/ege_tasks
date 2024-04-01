fe55644f5a45dcca439c01250bb397441c7b92a9
ispromashka <ispromadhka@gmail.com>
2024-04-01 14:42:14 +0300

    19-21 example

---
+++ tasks/theory of games/19-21.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+
+# системам ходов. изменения могут быть только в h и в return .
+def move(h):
+    a, b = h
+    return (a + 1, b), (a, b + 1), (a * 4, b), (a, b * 4)
+
+#алгоритм игры
+# в может поменяться строчка if sum(h) ... т.к могут быть 1 или 2 кучи
+@lru_cache(None)
+def game(h):
+    a, b = h
+    if sum(h) >= N:
+        return 'W'
+    if any(game(m) == 'W' for m in move(h)):
+        return 'P1'
+    if all(game(m) == 'P1' for m in move(h)):
+        return 'V1'
+    if any(game(m) == 'V1' for m in move(h)):
+        return 'P2'
+    if all(game(m) == 'P1' or game(m) == 'P2' for m in move(h)):
+        return 'V2'
+
+# цикл вывода данных
+for s in range(1, 94):
+    h = (6, s)
+    if game(h) is not None and game(h) != 'P1':
+        print(s, game(h))
