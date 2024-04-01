0147313a73986addd09dd806d2c394482dca9610
ispromashka <ispromadhka@gmail.com>
2024-04-01 13:12:55 +0300

    8 example

---
+++ tasks/8/8.py
@@ -0,0 +1,15 @@
+from itertools import product,combinations,permutations
+# func: product,combinations,permutations - методы для работы с комбинаторикой
+# product,combinations - все возмомжные перестановки WORD
+# permutations - все уникальные перетановки
+
+c=0
+# counter
+
+for i in func('WORD',repeat или r=5):
+    i=''.join(i)
+    c+=1
+    print(c,i)
+
+
+# made by isp
