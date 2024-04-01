479c7aeb308229e6ff50e740ec096812b4d0c375
shadowplay1 <shadowplay483@gmail.com>
2024-04-01 10:11:01 +0300

    init

---
+++ tasks/2/2.py
@@ -0,0 +1,8 @@
+expression = True # выражение
+
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            for w in range(0, 2):
+                if expression == 1:
+                    print(x, y, z, w)
